4c16381d9e6da56fc6968b18f8aab7f903164272
squidKnight <aliebers@terpmail.umd.edu>
2024-04-09 22:33:26 -0400

    Added arc function

--- Final/src/main.cpp
+++ Final/src/main.cpp
@@ -1,49 +1,143 @@
+#include "move.h"
+
 #include <Arduino.h>
 #include <Adafruit_MCP3008.h>
 #include <Adafruit_MPU6050.h>
 #include <Encoder.h>
 
-// IMU (rotation rate and acceleration)
+Adafruit_MCP3008 adc1;
+Adafruit_MCP3008 adc2;
 Adafruit_MPU6050 mpu;
 
-#include "move.h"
+const float M_I_COUNTS_TO_A = (3.3 / 1024.0) / 0.120;
+
+const unsigned int PWM_VALUE = 512; // Max PWM given 8 bit resolution
+
+bool status = false;
+
+float error;
+float last_error;
+float total_error;
+
+int base_pid = 450;
+
+float Kp = 0.2;
+float Kd = 10;
+float Ki = 0;
+
+const unsigned int ADC_1_CS = 2;
+const unsigned int ADC_2_CS = 17;
+
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
+
+const unsigned int M1_I_SENSE = 35;
+const unsigned int M2_I_SENSE = 34;
+
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 10;
+
+const int M_PWM_FREQ = 5000;
+const int M_PWM_BITS = 8;
+//const unsigned int MAX_PWM_VALUE = 512; // Max PWM given 8 bit resolution
+
+float METERS_PER_TICK = (3.14159 * 0.032) / 360.0; // The diameter of the wheel is 0.032 not 0.031
+float TURNING_RADIUS_METERS = 4.3 / 100.0; // Wheels are about 4.3 cm from pivot point
 
 void setup() {
-  const unsigned int ADC_1_CS = 2;
-  const unsigned int ADC_2_CS = 17;
-
-  // fix the pin that is set high during boot
+  // Stop the right motor by setting pin 14 low
+  // this pin floats high or is pulled
+  // high during the bootloader phase for some reason
   pinMode(14, OUTPUT);
   digitalWrite(14, LOW);
   delay(100);
 
-  // Disalbe the lightbar ADC chips so they don't hold the SPI bus used by the IMU
-  pinMode(ADC_1_CS, OUTPUT);
-  pinMode(ADC_2_CS, OUTPUT);
-  digitalWrite(ADC_1_CS, HIGH);
-  digitalWrite(ADC_2_CS, HIGH);
-
-  // baud rate needed to be adjusted from 115200 used previously to 9600 for some reason
   Serial.begin(9600);
 
-  // Try to initialize!
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      ledcWriteNote(0, NOTE_C, 4);
-      delay(500);
-      ledcWriteNote(0, NOTE_G, 4);
-      delay(500);
-    }
-  }
-  Serial.println("MPU6050 Found!");
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_1000_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
+  // configure motor pins
+  ledcSetup(M1_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M1_IN_2_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_2_CHANNEL, freq, resolution);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+
+  pinMode(M1_I_SENSE, INPUT);
+  pinMode(M2_I_SENSE, INPUT);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(".");
+  Encoder enc1(M1_ENC_A, M1_ENC_B);
+  Encoder enc2(M2_ENC_A, M2_ENC_B);
+  
+  delay(3000);
+
+  arc(0.001, 90, 512, 0, enc1, enc2);
+  delay(500);
+  arc(0.1, 180, 512, 1, enc1, enc2);
+  delay(500);
+  arc(0.001, 90, 512, 0, enc1, enc2);
+
+  /*
+  delay(3000);
+  spin(90, 480, 1, enc1, enc2);
+  delay(500);
+  spin(90, -480, 1, enc1, enc2);
+  delay(500);
+  spin(180, 480, 1, enc1, enc2);
+  delay(500);
+  spin(180, -480, 1, enc1, enc2);
+  delay(500);
+  spin(360, 480, 1, enc1, enc2);
+  delay(500);
+  */
+
+
+  /* draft straight move function from 04/01/2024 session:
+
+  // Create the encoder objects after the motor has
+  // stopped, else some sort exception is triggered
+  Encoder enc1(M1_ENC_A, M1_ENC_B);
+  Encoder enc2(M2_ENC_A, M2_ENC_B);
+
+  enc1.write(0);
+  enc2.write(0);
+
   delay(1000);
+
+  while(true) {
+    long enc1_value = enc1.read();
+    long enc2_value = enc2.read();
+
+    int t_start = micros();
+    int t_end = micros();
+
+    error = enc1_value + enc2_value;
+    total_error += error;
+
+    int pid_value = Kp*error + Kd*(error-last_error) + Ki*total_error;
+    int right_motor = base_pid + pid_value;
+    int left_motor = base_pid - pid_value;
+
+    last_error = error;
+
+    M1_forward(min(left_motor,512));
+    M2_forward(min(right_motor,512));
+
+      Serial.print(enc1_value);
+      Serial.print("\t");
+      Serial.print(enc2_value);
+      Serial.print("\t");
+      Serial.print(error);
+      Serial.println();
+
+  }
+  */
 }
