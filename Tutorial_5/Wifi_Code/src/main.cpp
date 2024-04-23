f80f8e0328730039c8570182e046c742b253c09e
PatrickMarinich <patrick.marinich@yahoo.com>
2024-04-23 11:12:46 -0400

    updated wifi code for turning

--- Tutorial_5/Wifi_Code/src/main.cpp
+++ Tutorial_5/Wifi_Code/src/main.cpp
@@ -17,14 +17,38 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiAP.h>
+#include "move.h"
 
 #define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
 
 // Set these to your desired credentials.
-const char *ssid = "yourAP";
-const char *password = "yourPassword";
+const char *ssid = "PatConnect";
+const char *password = "YourPassword";
 
 WiFiServer server(80);
+
+const unsigned int ADC_1_CS = 2;
+const unsigned int ADC_2_CS = 17;
+
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
+
+const unsigned int M1_I_SENSE = 35;
+const unsigned int M2_I_SENSE = 34;
+
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 10;
+
+const int M_PWM_FREQ = 5000;
+const int M_PWM_BITS = 8;
+//const unsigned int MAX_PWM_VALUE = 512; // Max PWM given 8 bit resolution
+
+float METERS_PER_TICK = (3.14159 * 0.032) / 360.0; // The diameter of the wheel is 0.032 not 0.031
+float TURNING_RADIUS_METERS = 4.3 / 100.0; // Wheels are about 4.3 cm from pivot point
+
 
 
 void setup() {
@@ -46,10 +70,37 @@ void setup() {
   server.begin();
 
   Serial.println("Server started");
+
+
+  //motor stuff
+    // Stop the right motor by setting pin 14 low
+  // this pin floats high or is pulled
+  // high during the bootloader phase for some reason
+
+
+  // configure motor pins
+  ledcSetup(M1_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M1_IN_2_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_2_CHANNEL, freq, resolution);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+
+  pinMode(M1_I_SENSE, INPUT);
+  pinMode(M2_I_SENSE, INPUT);
+
+
 }
 
 void loop() {
   WiFiClient client = server.available();   // listen for incoming clients
+
+  Encoder enc1(M1_ENC_A, M1_ENC_B);
+  Encoder enc2(M2_ENC_A, M2_ENC_B);
+  
 
   if (client) {                             // if you get a client,
     Serial.println("New Client.");           // print a message out the serial port
@@ -70,9 +121,10 @@ void loop() {
             client.println();
 
             // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-            client.print("Hello World <br>");
+            //client.print("Click <a href=\"/RIGHT\">here</a> to turn Right.<br>");
+            //client.print("Click <a href=\"/LEFT\">here</a> to turn Left.<br>");
+            //client.print("Hello World <br>");
+            client.print("Hello World");
 
             // The HTTP response ends with another blank line:
             client.println();
@@ -84,16 +136,43 @@ void loop() {
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
           currentLine += c;      // add it to the end of the currentLine
         }
-
+        
         // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+        if (currentLine.endsWith("GET /RIGHT")) {
+          //digitalWrite(LED_BUILTIN, HIGH);// GET /H turns the LED on
+
+
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          // the content of the HTTP response follows the header:
+          client.print("RIGHT<br>");
+          // The HTTP response ends with another blank line:
+          client.println();
+
+
+          //spin(90, 400, false,  enc1, enc2); //turn right
+
+          //currentLine = "";
+
+        } else if (currentLine.endsWith("GET /LEFT")) {
+          //digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
+
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          // the content of the HTTP response follows the header:
+          client.print("LEFT<br>");
+          // The HTTP response ends with another blank line:
+          client.println();   
+
+
+          //spin(90, -400, true,  enc1, enc2);
+     
         }
       }
     }
+
     // close the connection:
     client.stop();
     Serial.println("Client Disconnected.");
